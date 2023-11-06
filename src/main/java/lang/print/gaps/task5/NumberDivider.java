fdb901db6d31a56e4aefadd4eea4aa049ac47b4f
Artur Semenas <uncelfuka@gmail.com>
2023-11-06 13:05:59 +0100

    Module #3. Task #5. Operators

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float res = toBeDivided/5.0f;
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        int res = first*second*third*third+second*third*second+third;
+        System.out.println(res);
     }
 }
